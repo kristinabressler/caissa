b613201e6e27eeb1a96780717b98e627cd6be26c
kikibres <kristinabressler@yahoo.com>
2020-07-12 20:45:18 -0400

    Updates

---
+++ precache-manifest.3d06925852b57d5ba90b2209c9536afb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d662c44bab2bcf5961bb3a7cf2eea8c8",
+    "url": "/caissa/index.html"
+  },
+  {
+    "revision": "8e51f4440e8b0a120355",
+    "url": "/caissa/static/css/main.c5f5fe92.chunk.css"
+  },
+  {
+    "revision": "d707bc117ca540377936",
+    "url": "/caissa/static/js/2.317d4a00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/caissa/static/js/2.317d4a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e51f4440e8b0a120355",
+    "url": "/caissa/static/js/main.e85ac674.chunk.js"
+  },
+  {
+    "revision": "6e508eb1cb6840a96a8e",
+    "url": "/caissa/static/js/runtime-main.ac4d74f6.js"
+  },
+  {
+    "revision": "fe5993b7e8f1567a2e4cb821b29b51b1",
+    "url": "/caissa/static/media/caissa-icon.fe5993b7.png"
+  }
+]);
